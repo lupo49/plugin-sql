602287e9b2b26daf6da9ad47100e2137e8cbf1e3
Andreas Gohr <andi@splitbrain.org>
2016-02-03 12:46:40 +0100

    Adjust method signatures to match parent

--- syntax.php
+++ syntax.php
@@ -65,7 +65,7 @@ class syntax_plugin_sql extends DokuWiki_Syntax_Plugin {
     /**
      * Handle the match
      */
-    function handle($match, $state, $pos, &$handler){
+    function handle($match, $state, $pos, Doku_Handler $handler){
         switch ($state) {
           case DOKU_LEXER_ENTER : 
 			$urn = property('db',$match);
@@ -94,7 +94,7 @@ class syntax_plugin_sql extends DokuWiki_Syntax_Plugin {
     /**
      * Create output
      */
-    function render($mode, &$renderer, $data) {
+    function render($mode, Doku_Renderer $renderer, $data) {
 		$renderer->info['cache'] = false;
 		
         if($mode == 'xhtml'){
